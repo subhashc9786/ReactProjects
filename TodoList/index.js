1db72ee2578f0e8becfddf4db1761c9d7532e72c
Subhash Chander <69232495+subhashc9786@users.noreply.github.com>
2022-03-24 10:58:26 +0530

    Add files via upload

---
+++ TodoList/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// import ToDoList from "./ToDoList";
+ReactDOM.render(<App />, document.getElementById("root"));
